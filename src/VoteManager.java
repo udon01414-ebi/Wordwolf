3f669b53dbbe00991896da46c34b33b1dec81f85
class <class@172.20.26.131>
2026-05-15 10:55:10 +0900

    k

---
+++ src/VoteManager.java
@@ -0,0 +1,21 @@
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class VoteManager {
+	int playerCount;
+	int wolfindex;
+	
+	int[]votes;
+	int currenfVote =0;
+	
+	JLabel label;
+	JTextField inpit;
+	JButton button;
+	
+	public VoteManager(int playerCount, int wolfIndex,
+            JLabel label, JTextField input, JButton button) {
+		
+	}
+
+}
